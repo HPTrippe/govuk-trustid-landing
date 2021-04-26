cc367caa143a2380008af6c0ba9f2876f382b9e6
HPTrippe <72393473+HPTrippe@users.noreply.github.com>
2021-04-26 18:15:52 +0100

    Change name and CTA

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Help service teams do the right checks',
+  serviceName: 'Digital identity',
 
   // Default port that prototype runs on
   port: '3000',
